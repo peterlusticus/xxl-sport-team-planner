f4c60750f05ec21141b4cf5cbd9221a46f7a0116
khliscs <hliscs.k@gmail.com>
2022-06-20 15:23:43 +0200

    trash change

--- xxl-sport-team-planner/src/screens/Home.js
+++ xxl-sport-team-planner/src/screens/Home.js
@@ -29,7 +29,7 @@ export default function ({ navigation }) {
             <Button
               text="Zum Kalender"
               onPress={() => {
-                navigation.navigate("Calendar");
+                navigation.navigate("Celendar");
               }}
               style={{
                 marginTop: 10,
