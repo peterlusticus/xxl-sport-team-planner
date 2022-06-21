ca09318f29f22e22d097fcdf790f0dd84053afae
Richard Peters <rpe@xima.de>
2022-06-21 23:11:56 +0200

    improve firebase calender

--- xxl-sport-team-planner-admin-dashboard-2/js/loadcalendar.js
+++ xxl-sport-team-planner-admin-dashboard-2/js/loadcalendar.js
@@ -15,6 +15,7 @@ function init() {
 }
 
 $(document).ready(function () {
+
     //load courses
     firebase.database().ref('courses').on('value', (snapshot) => {
         if (snapshot.exists()) {
@@ -24,7 +25,13 @@ $(document).ready(function () {
                 const cours = data[i];
                 $('#calendar-cours-table').append('<tr class="selectable-row" id="' + i + '"> <td class="is-checkbox-cell"> <label class="b-checkbox checkbox"> <input type="checkbox" value="false"> <span class="check"></span> </label> </td> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + cours.name + '</td> </tr>')
             }
-            $('#0').click()
+
+            const selected_cours = localStorage.getItem("selected-cours")
+            if (selected_cours === null || selected_cours === 0) {
+                $('#0').click()
+            } else {
+                $('#' + selected_cours).click()
+            }
         }
     });
 
@@ -33,37 +40,11 @@ $(document).ready(function () {
     $("body").on("click", ".selectable-row", function () {
         const coursidx = $(this).attr("id")
         $("td.is-checkbox-cell > label > input[type=checkbox]").prop('checked', false);
-        $("#"+coursidx+" > td.is-checkbox-cell > label > input[type=checkbox]").prop('checked', true);
-        //on add event
-        scheduler.createDataProcessor(function (entity, action, data, id) {
-            switch (action) {
-                case "create":
-                    console.log(coursidx)
-                    firebase.database().ref('courses/' + coursidx + '/events').limitToLast(1).once('value').then(function(snapshot) {
-                        let num = 0
-                        if (snapshot.exists())  {
-                            num=parseInt(Object.keys(snapshot.val())[0])+1
-                        }
-                        firebase.database().ref('courses/' + coursidx + '/events/' + num).set({
-                            text: data.text,
-                            start_date: data.start_date,
-                            end_date: data.end_date
-                        });
-                    });
-                    
-
-                case "update":
-
-                    break;
-                case "delete":
-
-                    break;
-            }
-        });
+        $("#" + coursidx + " > td.is-checkbox-cell > label > input[type=checkbox]").prop('checked', true);
 
         //load events from firebase
         firebase.database().ref('courses/' + coursidx + '/events').on('value', (snapshot) => {
-            if (snapshot.exists())  {
+            if (snapshot.exists()) {
                 const data = snapshot.val();
                 scheduler.clearAll();
                 for (let i = 0; i < data.length; i++) {
@@ -75,5 +56,34 @@ $(document).ready(function () {
             }
         });
 
+        //set cookie
+        localStorage.setItem("selected-cours", coursidx);
     })
+
+    //on add event
+    scheduler.createDataProcessor(function (entity, action, data, id) {
+        switch (action) {
+            case "create":
+                console.log(localStorage.getItem("selected-cours"))
+                firebase.database().ref('courses/' + localStorage.getItem("selected-cours") + '/events').limitToLast(1).once('value').then(function (snapshot) {
+                    let event_num = 0
+                    if (snapshot.exists()) {
+                        event_num = parseInt(Object.keys(snapshot.val())[0]) + 1
+                    }
+                    console.log(localStorage.getItem("selected-cours"))
+                    firebase.database().ref('courses/' + localStorage.getItem("selected-cours") + '/events/' + event_num).set({
+                        text: data.text,
+                        start_date: data.start_date,
+                        end_date: data.end_date
+                    });
+                });
+
+            case "update":
+
+                break;
+            case "delete":
+
+                break;
+        }
+    });
 })
