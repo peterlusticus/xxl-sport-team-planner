bf4d7977c576f0423ee2d35a6bd141dadbbe0939
khliscs <hliscs.k@gmail.com>
2022-07-18 11:18:38 +0200

    intermediat state momen js

--- xxl-sport-team-planner/src/screens/Profile.js
+++ xxl-sport-team-planner/src/screens/Profile.js
@@ -14,7 +14,6 @@ export default () => {
 	const [eventModalDataTrainerNames, setEventModalDataTrainerNames] = useState({});
 
 	const { currentUser } = getAuth();
-	const {moment} = Boolean;
 
 	useEffect(() => {
 		if (currentUser) {
@@ -42,17 +41,19 @@ export default () => {
 	const [visible, setVisible] = useState(false);
 
 	function showModal(date) {
-		setEventModalData(searchEventByDate(date.dateString))
-		let trainer = ""
-		const userRef = ref(db, "users");
-		onValue(userRef, (snapshot) => {
-			if (snapshot.exists()) {
-				var data = snapshot.val();
-				trainer = data[eventModalData['trainer']] === undefined ? "" : data[eventModalData['trainer']]['vorname']
-			}
-		});
-		setEventModalDataTrainerNames(trainer)
-		setVisible(true)
+		if (searchEventByDate(date.dateString)) {
+			setEventModalData(searchEventByDate(date.dateString))
+			let trainer = "Du"
+			const userRef = ref(db, "users");
+			onValue(userRef, (snapshot) => {
+				if (snapshot.exists()) {
+					var data = snapshot.val();
+					trainer = data[eventModalData['trainer']] === undefined ? "" : data[eventModalData['trainer']]['vorname']
+				}
+			});
+			setEventModalDataTrainerNames(trainer)
+			setVisible(true)
+		}
 	}
 
 	function searchEventByDate(date) {
