b059b2f9903764cbab56a50a650331409308fcf6
Richard Peters <rpe@xima.de>
2022-07-18 11:31:14 +0200

    delete courses

--- xxl-sport-team-planner-admin-dashboard-2/js/loadcourses.js
+++ xxl-sport-team-planner-admin-dashboard-2/js/loadcourses.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
     firebase.database().ref('courses').on('value', (snapshot) => {
         if (snapshot.exists()) {
             const data = snapshot.val();
-            num = data.length
             $('#cours-table-klettern').empty();
             $('#cours-table-bouldern').empty();
-            for (let i = 0; i < data.length; i++) {
-                const cours = data[i];
-                if (cours.sportart == "Klettern") {
-                    $('#cours-table-klettern').append('<tr> <td class="is-checkbox-cell"> <label class="b-checkbox checkbox"> <input type="checkbox" value="false"> <span class="check"></span> </label> </td> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + cours.name + '</td> <td data-label="Teilnehmerzahl">' + cours.attendance + '</td> <td data-label="Altersklasse">' + cours.age + '</td> <td data-label="Erstellt_am">'+cours.datum+'</td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary btn-edit" id='+i+' type="button"> <span class="icon"><i class="mdi mdi-eye"></i></span> </button> <button class="button is-small is-danger jb-modal btn-delete" data-target="modal-delete-cours" id='+i+' type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
-                } else {
-                    $('#cours-table-bouldern').append('<tr> <td class="is-checkbox-cell"> <label class="b-checkbox checkbox"> <input type="checkbox" value="false"> <span class="check"></span> </label> </td> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + cours.name + '</td> <td data-label="Teilnehmerzahl">' + cours.attendance + '</td> <td data-label="Altersklasse">' + cours.age + '</td> <td data-label="Erstellt_am">'+cours.datum+'</td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary btn-edit" id='+i+' type="button"> <span class="icon"><i class="mdi mdi-eye"></i></span> </button> <button class="button is-small is-danger jb-modal btn-delete" data-target="modal-delete-cours" id='+i+' type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
+            //for (let i = 0; i < data.length; i++) {
+            for (const key in data) {
+                if (Object.hasOwnProperty.call(data, key)) {
+                    const cours = data[key];
+                    if (cours.sportart == "Klettern") {
+                        $('#cours-table-klettern').append('<tr> <td class="is-checkbox-cell"> <label class="b-checkbox checkbox"> <input type="checkbox" value="false"> <span class="check"></span> </label> </td> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + cours.name + '</td> <td data-label="Teilnehmerzahl">' + cours.attendance + '</td> <td data-label="Altersklasse">' + cours.age + '</td> <td data-label="Erstellt_am">' + cours.datum + '</td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary btn-edit" id=' + key + ' type="button"> <span class="icon"><i class="mdi mdi-eye"></i></span> </button> <button class="button is-small is-danger jb-modal btn-delete" data-target="modal-delete-cours" id=' + key + ' type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
+                    } else {
+                        $('#cours-table-bouldern').append('<tr> <td class="is-checkbox-cell"> <label class="b-checkbox checkbox"> <input type="checkbox" value="false"> <span class="check"></span> </label> </td> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + cours.name + '</td> <td data-label="Teilnehmerzahl">' + cours.attendance + '</td> <td data-label="Altersklasse">' + cours.age + '</td> <td data-label="Erstellt_am">' + cours.datum + '</td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary btn-edit" id=' + key + ' type="button"> <span class="icon"><i class="mdi mdi-eye"></i></span> </button> <button class="button is-small is-danger jb-modal btn-delete" data-target="modal-delete-cours" id=' + key + ' type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
+                    }
                 }
             }
         } else {
@@ -19,15 +21,15 @@ $(document).ready(function () {
         }
     });
 
-    $('body').on('click', '.btn-edit', function(){
+    $('body').on('click', '.btn-edit', function () {
         const id = $(this).attr('id')
-        firebase.database().ref('courses/'+id).once('value').then(function (snapshot) {
+        firebase.database().ref('courses/' + id).once('value').then(function (snapshot) {
             console.log(snapshot.val())
             $("#tfKursname").val(snapshot.val().name)
             $("#tfAltersklasse").val(snapshot.val().age)
             $("#tfTeilnehmerzahl").val(snapshot.val().attendance)
 
-            $('#btnSave').click(function(){
+            $('#btnSave').click(function () {
                 console.log(id)
                 firebase.database().ref('courses/' + id).update({
                     name: $("#tfKursname").val(),
@@ -35,25 +37,25 @@ $(document).ready(function () {
                     attendance: $("#tfTeilnehmerzahl").val(),
                 });
             })
-            
-         })
+
+        })
         $('#modal-edit-cours').addClass('is-active');
-        
+
     })
 
-    $('body').on('click', '.btn-delete', function(){
+    $('body').on('click', '.btn-delete', function () {
         const id = $(this).attr('id');
-        $('#btnDelete').click(function(){
+        $('#btnDelete').click(function () {
             firebase.database().ref('courses/' + id).set({});
         })
         $('#modal-delete-cours').addClass('is-active');
-        
+
     })
 
     $("#btnKursHinzufügenKlettern").click(function () {
         const d = new Date()
-        const datum = d.getDate()+"."+d.getMonth()+"."+d.getFullYear();
-        firebase.database().ref('courses/' + num).set({
+        const datum = d.getDate() + "." + d.getMonth() + "." + d.getFullYear();
+        firebase.database().ref('courses/' + uuidv4()).set({
             name: $("#tfKursnameKlettern").val(),
             attendance: $("#tfTeilnehmerzahlKlettern").val(),
             age: $("#tfAltersklasseKlettern").val(),
@@ -64,8 +66,8 @@ $(document).ready(function () {
 
     $("#btnKursHinzufügenBouldern").click(function () {
         const d = new Date()
-        const datum = d.getDate()+"."+d.getMonth()+"."+d.getFullYear();
-        firebase.database().ref('courses/' + num).set({
+        const datum = d.getDate() + "." + d.getMonth() + "." + d.getFullYear();
+        firebase.database().ref('courses/' + uuidv4()).set({
             name: $("#tfKursnameBouldern").val(),
             attendance: $("#tfTeilnehmerzahlBouldern").val(),
             age: $("#tfAltersklasseBouldern").val(),
@@ -73,4 +75,10 @@ $(document).ready(function () {
             datum: datum
         });
     })
+
+    function uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
 });
--- xxl-sport-team-planner-admin-dashboard-2/js/loademployees.js
+++ xxl-sport-team-planner-admin-dashboard-2/js/loademployees.js
@@ -1,14 +1,32 @@
 $(document).ready(function () {
-    
-
     firebase.database().ref('users').on('value', (snapshot) => {
         if (snapshot.exists()) {
             const data = snapshot.val();
            $('#trainer-table').empty();
-            for (const key in data) {
-                if (Object.hasOwnProperty.call(data, key)) {
-                    const trainer = data[key];
-                    $('#trainer-table').append('<tr> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + trainer.vorname + " " + trainer.nachname + '</td> <td data-label="Kurse">Todo</td> <td data-label="Verhältnis">' + trainer.verhaeltnis + '</td> <td data-label="Gehalt">' + trainer.gehalt + '€/h </td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary jb-modal btn-edit" id='+key+' data-target="modal-edit-employee" type="button"> <span class="icon"> <i class="mdi mdi-eye"></i> </span> </button> <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
+            for (const trainerkey in data) {
+                if (Object.hasOwnProperty.call(data, trainerkey)) {
+                    const trainer = data[trainerkey];
+                    //get courses by searching events
+                    firebase.database().ref('courses').once('value').then(function (snapshot) {
+                        let coursstring = ""
+                        for (const courskey in snapshot.val()) {
+                            if (Object.hasOwnProperty.call(snapshot.val(), courskey)) {
+                                const cours = snapshot.val()[courskey];
+                                for (let i = 0; i < cours['events'].length; i++) {
+                                    const event = cours['events'][i];
+                                    if (event['trainer'] === trainerkey ) {
+                                        let comma = ""
+                                        if(coursstring !== ""){
+                                            comma = ", "
+                                        }
+                                        coursstring = coursstring + comma + cours['name']
+                                    }
+                                }
+                            }
+                        }
+                        $('#trainer-table').append('<tr> <td class="is-image-cell"> <div class="image"> <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="is-rounded"> </div> </td> <td data-label="Name">' + trainer.vorname + " " + trainer.nachname + '</td> <td data-label="Kurse">'+coursstring+'</td> <td data-label="Verhältnis">' + trainer.verhaeltnis + '</td> <td data-label="Gehalt">' + trainer.gehalt + '€/h </td> <td class="is-actions-cell"> <div class="buttons is-right"> <button class="button is-small is-primary jb-modal btn-edit" id='+trainerkey+' data-target="modal-edit-employee" type="button"> <span class="icon"> <i class="mdi mdi-eye"></i> </span> </button> <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button"> <span class="icon"><i class="mdi mdi-trash-can"></i></span> </button> </div> </td> </tr>')
+                    })
+                    
                 }
             }
         }else{
